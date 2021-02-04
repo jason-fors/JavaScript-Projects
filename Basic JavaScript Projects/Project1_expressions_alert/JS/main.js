5eed89f22b9460f9da1efa72d0dacdd603953024
Jason-fors <jasonfors@hotmail.com>
2021-02-04 15:03:15 -0500

    Add js project 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+
+
+
+
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
+
+3+3 // Writing an expression. Not sure what the point is.
+
+var greet="Hello ", person="Jeff"; // Declare and assign variables.
+greet = greet.fontcolor("blue");
+person = person.fontcolor("red");
+
+document.write(greet + person);
+
+warning = "Enter at your own risk!";
+window.alert(warning);
+
+document.write(2+2); // Writing an expression. Not sure what the point is.
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # JavaScript-Projects
+These are some pages I made while first learning JavaScript.
+
+## Projects
+* Practice and Learning Pages
+
+### Practice and Learning Pages
+The rest are numberous small practice programs. 
+
